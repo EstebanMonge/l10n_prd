8cb178b01268180f4f2faa2055ddf62d4d033484
Esteban Monge <emonge@gbm.net>
2019-07-03 16:48:50 -0600

    upgrade db script

---
+++ upgrade_odoo_dbs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for i in $(sudo -u postgres psql -c '\l' | grep odoo | awk '{print $1}')
+do
+        echo "Upgrading database $i"
+        sudo -u odoo /usr/bin/odoo --stop-after-init -c /etc/odoo/odoo.conf -d $i -u all --logfile=/tmp/odoo_$i
+done
