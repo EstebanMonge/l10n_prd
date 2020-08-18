662b1894cc862c851a23b9ea521dd7dcfa36d166
Esteban Monge <estebanmonge@riseup.net>
2020-08-17 19:59:00 -0600

    more fixes to install_prd script

--- install_prd.sh
+++ install_prd.sh
@@ -1,4 +1,10 @@
-sudo apt-get install certbot python-certbot-nginx postgresql-client 
+sudo apt-get -y install vim git certbot python3-pip python-certbot-nginx postgresql-client 
+sudo useradd -md /home/odoo odoo
+sudo apt -y install python3-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev git python3-dev libpq-de
+sudo rm wkhtmltox_0.12.5-1.buster_amd64.deb
+sudo wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.buster_amd64.deb
+sudo dpkg -i wkhtmltox_0.12.5-1.buster_amd64.deb
+sudo apt-get -f install -y
 sudo -u odoo pip3 install xmlsig
 sudo -u odoo pip3 install pyOpenSSL
 sudo -u odoo pip3 install jsonschema
@@ -9,6 +15,8 @@ sudo -u odoo pip3 install xlwt
 rm show_open_pos.sh*
 rm upgrade_odoo_dbs.sh*
 rm -r l10n_cr server-tools partner-contact bank-payment community-data-files odooapps pos account-financial-reporting server-ux reporting-engine EstebanMonge web l10n_cr_priv manufacture
+sudo git clone --depth 1 --branch 12.0 https://www.github.com/odoo/odoo.git /opt/odoo
+sudo chown -R odoo.odoo /opt/odoo/
 cd /opt/odoo/addons/
 rm -r cr_customer_query cr_electronic_invoice cr_electronic_invoice_qweb_fe cr_electronic_invoice_pos l10n_cr_country_codes res_currency_cr_adapter onchange_helper base_vat_sanitized account_payment_partner uom_unece account_tax_unece base_unece account_payment_mode accounting_pdf_reports om_account_accountant om_account_asset om_account_budget l10n_cr_hacienda_info_query pos_ticket_logo account_financial_report date_range report_xlsx om_pos_service_charge parking basic_hms l10n_cr_hacienda_info_query web_responsive parking_report cr_electronic_invoice_pos_qweb_fe product_quick_bom
 cd $HOME
@@ -22,14 +30,14 @@ git clone --branch 12.0 --depth 1 --single-branch https://github.com/OCA/communi
 git clone --branch 12.0 --depth 1 --single-branch https://github.com/OCA/web.git
 git clone --branch 12.0 --depth 1 --single-branch https://github.com/OCA/manufacture.git
 git clone --branch 12.0 --depth 1 --single-branch https://github.com/odoomates/odooapps.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/edi.git
+git clone --branch 12.0 --depth 1 --single-branch https://github.com/OCA/edi.git
 mkdir EstebanMonge
 cd EstebanMonge
 git clone --branch 12.0 --depth 1 --single-branch https://github.com/EstebanMonge/odooapps.git
-git clone --branch 12.0 --depth 1 --single-branch https://github.com/EstebanMonge/basic_hms.git 
-git clone --branch 12.0 --depth 1 --single-branch https://github.com/EstebanMonge/l10n_cr_priv.git 
-git clone https://github.com/EstebanMonge/parking.git 
-git clone https://github.com/EstebanMonge/parking_report.git
+git clone --branch 12.0 --depth 1 --single-branch https://EstebanMonge@github.com/EstebanMonge/basic_hms.git 
+git clone --branch 12.0 --depth 1 --single-branch https://EstebanMonge@github.com/EstebanMonge/l10n_cr_priv.git 
+git clone https://EstebanMonge@github.com/EstebanMonge/parking.git 
+git clone https://EstebanMonge@github.com/EstebanMonge/parking_report.git
 git clone https://github.com/EstebanMonge/prt_report_attachment_preview.git
 git clone https://github.com/EstebanMonge/custom_pos_keyboard_shortcut
 git clone https://github.com/EstebanMonge/custom_pos_size
